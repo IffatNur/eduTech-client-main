1cb385a4f04574760694ee185f0061db2bad0f8d
IffatNur <iffatnoorshad00@gmail.com>
2022-10-26 21:00:28 +0600

    react to pdf applied

--- src/components/CheckOut/CheckOut.js
+++ src/components/CheckOut/CheckOut.js
@@ -5,10 +5,10 @@ const CheckOut = () => {
     const details = useLoaderData();
     return (
       <div>
-        <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-14 bg-cyan-300">
+        <div className="card w-96 shadow-xl mx-auto mt-14 bg-cyan-300">
           <div className="card-body text-center">
             <div className="card-actions justify-end "></div>
-            <h1 className="text-3xl">Congratutations! </h1>
+            <h1 className="text-3xl">Congratulations! </h1>
             <small>You have got premium access of the course below</small>
             <p>ID no : {details.id}</p>
             <p>
--- src/components/CourseDetails/CourseDetails.js
+++ src/components/CourseDetails/CourseDetails.js
@@ -1,50 +1,52 @@
 import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import { FaStar, FaStarHalfAlt, FaFileDownload } from "react-icons/fa";
-import { createRef } from 'react';
-import ReactToPdf from "react-to-pdf";
-import { useContext } from 'react';
+import { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
 
 
 
 const CourseDetails = () => {
-  const ref = createRef();
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content:()=> componentRef.current,
+    documentTitle: 'Course-details',
+    onAfterPrint: ()=> alert('Print success')
+  })
     const singleCourse = useLoaderData();
     const {id, code, course, details, instructor, rating, img } = singleCourse;
-    console.log(singleCourse);
+    
     return (
       <div>
         <div className="navbar bg-primary text-white">
           <div className="flex-1">
-            <ReactToPdf targetRef={ref} filename="code-example.pdf">
-              {({ toPdf }) => {
-                <Link
-                  onClick={toPdf}
-                  className="btn btn-ghost normal-case text-xl"
-                >
-                  Download File{" "}
-                  <FaFileDownload className="text-error"></FaFileDownload>
-                </Link>;
-              }}
-            </ReactToPdf>
+            <button
+              onClick={handlePrint}
+              className="btn btn-ghost normal-case text-xl"
+            >
+              Download File{" "}
+              <FaFileDownload className="text-error"></FaFileDownload>
+            </button>
           </div>
         </div>
         <div className="card lg:card-side shadow-xl lg:w-3/4 mx-auto mt-24 bg-sky-200">
           <figure className="w-full">
             <img className="h-full" src={img} alt="Album" />
           </figure>
-          <div ref={ref} className="card-body">
-            <h2 className="card-title">
-              <b>
-                {code + " : "} <i>{course}</i>
-              </b>{" "}
-            </h2>
-            <p>
-              <i>Details :</i> {details}
-            </p>
-            <p>
-              <b>Instructor : {instructor}</b>
-            </p>
+          <div className="card-body">
+            <div ref={componentRef}>
+              <h2 className="card-title">
+                <b>
+                  {code + " : "} <i>{course}</i>
+                </b>{" "}
+              </h2>
+              <p>
+                <i>Details :</i> {details}
+              </p>
+              <p>
+                <b>Instructor : {instructor}</b>
+              </p>
+            </div>
             <div className="card-actions justify-between">
               <span className="flex text-yellow-400 items-center">
                 <span className="text-black">
